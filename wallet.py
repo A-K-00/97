fc0b4e54a0f4f0b21e4cc25bdadc3afb169ef8c7
A-K-00 <159860135+A-K-00@users.noreply.github.com>
2024-07-25 13:09:20 -0400

    Add files via upload

---
+++ wallet.py
@@ -0,0 +1,20 @@
+from web3 import Web3
+
+ganacheUrl = "http://127.0.0.1:7545" 
+web3 = Web3(Web3.HTTPProvider(ganacheUrl))
+
+class Account():
+    def __init__(self):
+        self.account = web3.eth.account.create()
+        self.address = self.account.address
+
+# Create a Wallet class
+
+class Wallet():
+    def checkConnection(self):
+        if web3.is_connected():
+           return True
+        else:
+            return False
+            
+    
